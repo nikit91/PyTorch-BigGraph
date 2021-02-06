ef812556c7d4fd38892deae168e16acc9f0e2b87
Nikit Srivastava <nikit91@gmail.com>
2021-02-06 21:33:43 +0100

    adding dbp config

---
+++ torchbiggraph/examples/configs/dbp_config_cpu.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE.txt file in the root directory of this source tree.
+
+
+def get_torchbiggraph_config():
+
+    config = dict(  # noqa
+        # I/O data
+        entity_path="data/dbpedia-2015-04",
+        edge_paths=[
+            "data/dbpedia-2015-04/train_partitioned",
+            "data/dbpedia-2015-04/valid_partitioned",
+            "data/dbpedia-2015-04/test_partitioned",
+        ],
+        checkpoint_path="model/dbp1504",
+        # Graph structure
+        entities={"all": {"num_partitions": 1}},
+        relations=[
+            {
+                "name": "all_edges",
+                "lhs": "all",
+                "rhs": "all",
+                "operator": "complex_diagonal",
+            }
+        ],
+        dynamic_relations=True,
+        # Scoring model
+        dimension=100,
+        global_emb=False,
+        comparator="dot",
+        # Training
+        num_epochs=5,
+        num_uniform_negs=100,
+        loss_fn="softmax",
+        lr=0.1,
+        regularization_coef=1e-3,
+        # Evaluation during training
+        eval_fraction=0,  # to reproduce results, we need to use all training data
+    )
+
+    return config
